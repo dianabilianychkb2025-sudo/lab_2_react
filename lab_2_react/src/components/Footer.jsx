aa704d3c8c731e66c392c22ca2a4177620d43eac
Діана Білянич <dianabilanic@Noutbuk-Diana.local>
2026-09-17 19:11:31 +0300

    Final version of React CV application

---
+++ lab_2_react/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Контакти:diana.bilianych.kb.2025@lpnu.ua</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ lab_2_react/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Діана Білянич</h1>
+      <p>Позиція: Junior Frontend Developer</p>
+    </header>
+  );
+}
+export default Header;
